d1c8eb7911e106edd261fa9c3e0e8e2a4150616a
Talut TAŞGIRAN <iletisim@taluttasgiran.com.tr>
2018-02-16 10:16:59 +0300

    update

--- SimpleCardView.js
+++ SimpleCardView.js
@@ -1,134 +1,139 @@
 import React from 'react';
-import {View, Dimensions, Text} from 'react-native';
+import { View, Dimensions, Text, Platform } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Ionicons'
 
 class CardViewWithIcon extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    // TODO: 1: Simgeye gölge ekleme ve gölge props ayarları - waiting
-    // TODO: 2: Animasyon ekleme - waiting
-    // TODO: 3: Simgeye border ekleme - done ✓
-    // TODO: 4: Boyut ayarları  - done ✓
-    // TODO: 5: Touchable area (onPress etc.) ekleme - waiting
-    // TODO: 6: elevation / shadowOffset farkını öğren (ios ve andorid)
+  // TODO: 1: Simgeye gölge ekleme ve gölge props ayarları - waiting
+  // TODO: 2: Animasyon ekleme - waiting
+  // TODO: 3: Simgeye border ekleme - done ✓
+  // TODO: 4: Boyut ayarları  - done ✓
+  // TODO: 5: Touchable area (onPress etc.) ekleme - waiting
 
-    render() {
-        const container = {
-            shadowColor: this.props.shadowColor,
-            shadowOpacity: this.props.shadowOpacity,
-            shadowRadius: this.props.shadowRadius,
-            backgroundColor: this.props.bgColor,
-            padding: this.props.padding,
-            margin: this.props.margin,
-            borderRadius: this.props.borderRadius,
-            width: (Dimensions.get('window').width / 2) - 20,
-            elevation: this.props.elevation,
+  render() {
+    const container = {
+      shadowColor    : this.props.shadowColor,
+      shadowOffset   : {
+        width : Platform.OS === "ios" ? this.props.shadowOffsetWidth : 0,
+        height: Platform.OS === "ios" ? this.props.shadowOffsetHeight : 0,
+      },
+      shadowOpacity  : this.props.shadowOpacity,
+      shadowRadius   : this.props.shadowRadius,
+      backgroundColor: this.props.bgColor,
+      padding        : this.props.padding,
+      margin         : this.props.margin,
+      borderRadius   : this.props.borderRadius,
+      width          : (Dimensions.get('window').width / 2) - 20,
+      elevation      : Platform.OS === "android" ? this.props.elevation : 0,
 
-        };
-        const icon = {
-            margin: 15,
-            borderWidth: 0.5,
-            borderColor: this.props.iconBgColor,
-            borderRadius: 40,
-            width: 80,
-            height: 80,
-            alignSelf: 'center',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: this.props.iconBgColor,
-        };
-        const title = {
-            alignSelf: 'center',
-            fontWeight: 'bold',
-            marginBottom: 10
-        };
-        const plainText = {
-            alignSelf: 'center',
-            marginBottom: 10,
-            fontWeight: '300',
-            textAlign: 'justify',
-            paddingLeft: 10,
-            paddingRight: 10,
-        };
+    };
+    const icon = {
+      margin         : 15,
+      borderWidth    : 0.5,
+      borderColor    : this.props.iconBgColor,
+      borderRadius   : 40,
+      width          : 80,
+      height         : 80,
+      alignSelf      : 'center',
+      justifyContent : 'center',
+      alignItems     : 'center',
+      backgroundColor: this.props.iconBgColor,
+    };
+    const title = {
+      alignSelf   : 'center',
+      fontWeight  : 'bold',
+      marginBottom: 10
+    };
+    const plainText = {
+      alignSelf   : 'center',
+      marginBottom: 10,
+      fontWeight  : '300',
+      textAlign   : 'justify',
+      paddingLeft : 10,
+      paddingRight: 10,
+    };
 
-        return (
-            <View style={{
-                backgroundColor: '#F5F5F5',
-                paddingTop: 40,
-                height: Dimensions.get('window').height,
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'flex-start',
-                alignContent: 'flex-start',
-                flexWrap: 'wrap'
-            }}>
-                <View style={container}>
-                    <View style={icon}>
-                        <Icon
-                            style={{
-                                textAlign: 'center',
-                            }}
-                            name={this.props.icon}
-                            size={this.props.iconSize}
-                            color={this.props.iconColor}
-                        />
-                    </View>
-                    <Text style={title}>{this.props.title}</Text>
-                    <Text style={plainText}>
-                        {this.props.content}
-                    </Text>
-                </View>
-            </View>
-        );
-    }
+    return (
+      <View style={ {
+        backgroundColor: '#F5F5F5',
+        paddingTop     : 40,
+        height         : Dimensions.get('window').height,
+        flex           : 1,
+        flexDirection  : 'row',
+        justifyContent : 'space-between',
+        alignItems     : 'flex-start',
+        alignContent   : 'flex-start',
+        flexWrap       : 'wrap'
+      } }>
+        <View style={ container }>
+          <View style={ icon }>
+            <Icon
+              style={ {
+                textAlign: 'center',
+              } }
+              name={ this.props.icon }
+              size={ this.props.iconSize }
+              color={ this.props.iconColor }
+            />
+          </View>
+          <Text style={ title }>{ this.props.title }</Text>
+          <Text style={ plainText }>
+            { this.props.content }
+          </Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 CardViewWithIcon.defaultProps = {
-    elevation: 1,
-    padding: 5,
-    margin: 10,
-    borderRadius: 3,
-    shadowColor: '#000000',
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-    bgColor: '#ffffff',
-    // ICON
-    icon: 'ios-bonfire-outline',
-    iconBgColor: '#3949AB',
-    iconColor: '#ffffff',
-    iconSize: 40,
-    // TITLE
-    title: 'LOREM IPSUM',
-    // CONTENT
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+  elevation         : 3,
+  padding           : 5,
+  margin            : 10,
+  borderRadius      : 3,
+  shadowColor       : '#000000',
+  shadowOpacity     : 0.3,
+  shadowRadius      : 3,
+  bgColor           : '#ffffff',
+  shadowOffsetWidth : 3,
+  shadowOffsetHeight: 3,
+  // ICON
+  icon              : 'ios-bonfire-outline',
+  iconBgColor       : '#3949AB',
+  iconColor         : '#ffffff',
+  iconSize          : 40,
+  // TITLE
+  title             : 'LOREM IPSUM',
+  // CONTENT
+  content           : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
 };
 
 CardViewWithIcon.propTypes = {
-    elevation: PropTypes.number,
-    padding: PropTypes.number,
-    margin: PropTypes.number,
-    borderRadius: PropTypes.number,
-    shadowColor: PropTypes.string,
-    shadowOpacity: PropTypes.number,
-    shadowRadius: PropTypes.number,
-    bgColor: PropTypes.string,
-    // ICON
-    icon: PropTypes.string,
-    iconBgColor: PropTypes.string,
-    iconColor: PropTypes.string,
-    iconSize: PropTypes.number,
-    // TITLE
-    title: PropTypes.string,
-    // CONTENT
-    content: PropTypes.string,
-
-
+  elevation         : PropTypes.number,
+  padding           : PropTypes.number,
+  margin            : PropTypes.number,
+  borderRadius      : PropTypes.number,
+  shadowColor       : PropTypes.string,
+  shadowOpacity     : PropTypes.number,
+  shadowRadius      : PropTypes.number,
+  shadowOffsetWidth : PropTypes.number,
+  shadowOffsetHeight: PropTypes.number,
+  bgColor           : PropTypes.string,
+  // ICON
+  icon              : PropTypes.string,
+  iconBgColor       : PropTypes.string,
+  iconColor         : PropTypes.string,
+  iconSize          : PropTypes.number,
+  // TITLE
+  title             : PropTypes.string,
+  // CONTENT
+  content           : PropTypes.string,
 };
 
 export {
-    CardViewWithIcon
+  CardViewWithIcon
 }
