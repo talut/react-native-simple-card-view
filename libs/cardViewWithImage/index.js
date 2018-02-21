b56524b6a6efeb0faa96fcdea230dc0e5bb1b992
Talut TAŞGIRAN <iletisim@taluttasgiran.com.tr>
2018-02-21 15:20:13 +0300

    update

--- libs/cardViewWithImage/index.js
+++ libs/cardViewWithImage/index.js
@@ -5,9 +5,29 @@ import PropTypes from 'prop-types'
 class CardViewWithImage extends Component {
 
   render() {
+    const container = {
+      shadowOffset   : {
+        width : Platform.OS === "ios" ? this.props.style.shadowOffsetWidth : 0,
+        height: Platform.OS === "ios" ? this.props.style.shadowOffsetWidth : 0,
+      },
+      backgroundColor: this.props.style.bgColor,
+      width          : this.props.style.width,
+      padding        : this.props.style.padding,
+      margin         : this.props.style.margin,
+      borderRadius   : this.props.style.borderRadius,
+      shadowColor    : this.props.style.shadowColor,
+      shadowOpacity  : this.props.style.shadowOpacity,
+      shadowRadius   : this.props.style.shadowRadius,
+      elevation      : Platform.OS === "android" ? this.props.style.elevation : 0,
+    };
     return (
-      <View>
-        <Text>CardViewWithImage</Text>
+      <View style={ container }>
+        <TouchableOpacity onPress={ this.props.onPress }>
+          <Image style={ {
+            width : 100,
+            height: 100
+          } } source={ {uri: 'https://placeimg.com/640/480/animals'} }/>
+        </TouchableOpacity>
       </View>
     )
   }
